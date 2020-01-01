f1c29541ba24516a265a82dd1d260201742e0854
cumet04 <medalhkr@gmail.com>
2020-01-01 04:37:49 +0000

    remove cdk
    There are not used ...

--- cdk/bin/app.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import cdk = require('@aws-cdk/core');
-import { AppStack } from '../lib/app-stack';
-
-const app = new cdk.App();
-new AppStack(app, 'AppStack');
